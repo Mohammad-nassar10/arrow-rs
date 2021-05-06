6d04387d59b0095b0a258ea4e0fb27b0b2507bc4
Mohammad-nassar10 <79787844+Mohammad-nassar10@users.noreply.github.com>
2021-05-06 16:04:18 +0300

    Update ffi.rs

--- arrow/src/ffi.rs
+++ arrow/src/ffi.rs
@@ -262,6 +262,7 @@ fn to_datatype(
                 .unwrap();
             // prevent a double free
             let name = ManuallyDrop::new(name);
+            println!("ggg in func to_datatype() schema = {:?},, children = {:?}" schema, schema.children);
             let mut V = Vec::new();
             V.push(Field::new(
                 &name,
